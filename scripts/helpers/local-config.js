5cd1ea6a51a34e57b3cb38e5bd25e535340048a9
pgmemk <ellen.katsnelson@lablz.com>
2017-07-12 22:41:52 -0400

    add fake aws config

--- scripts/helpers/local-config.js
+++ scripts/helpers/local-config.js
@@ -1,5 +1,7 @@
 require('dynogels').AWS.config.update({
   // localstack
   endpoint: 'http://localhost:4569',
-  region: 'us-east-1'
+  region: 'us-east-1',
+  accessKeyId: 'YOURKEY',
+  secretAccessKey: 'YOURSECRET',
 })
