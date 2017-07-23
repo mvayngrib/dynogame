6327d51e55ca7322da0a765934b217e26cd24f7f
pgmemk <ellen.katsnelson@lablz.com>
2017-07-22 20:36:48 -0400

    fix loading fixtures

--- example/load-fixtures.js
+++ example/load-fixtures.js
@@ -12,13 +12,21 @@ const tables = require('@tradle/dynamodb')
 co(function* () {
   const time = String(1499486259331)
   let i = 0
-  for (const fixture of fixtures) {
+  let saved = 0
+
+  setInterval(function () {
+    console.log(`put ${saved}/${fixtures.length} items`)
+  }, 1000).unref()
+
+  yield objects.set(fixtures)
+  yield fixtures.map(co(function* (fixture) {
     const type = fixture._t
+    i++
     if (!fixture._time) {
-      fixture._time = time + (i++)
+      fixture._time = time + i
     }
 
-    yield objects.put(fixture)
     yield tables[type].create(fixture)
-  }
+    saved++
+  }))
 })().catch(console.error)
--- example/objects.js
+++ example/objects.js
@@ -1,18 +1,26 @@
+const path = require('path')
+const pify = require('pify')
+const fs = pify(require('fs'))
 const co = require('co').wrap
-const low = require('lowdb')
-const db = low('objects.json')
-db.defaults({})
-  .write()
+const OBJECTS_FILE_PATH = path.resolve(process.cwd(), 'objects.json')
+let objects
+try {
+  objects = require(OBJECTS_FILE_PATH)
+} catch (err) {
+  objects = {}
+}
 
 module.exports = {
-  put: co(function* (wrapper) {
-    db.set(wrapper._link, wrapper)
-      .write()
+  set: co(function* (fixtures) {
+    objects = {}
+    fixtures.forEach(fixture => {
+      objects[fixture._link] = fixture
+    })
 
-    return wrapper
+    yield fs.writeFile(OBJECTS_FILE_PATH, JSON.stringify(objects, null, 2))
   }),
   get: co(function* (link) {
-    const val = db.get(link).value()
+    const val = objects[link]
     if (!val) {
       throw new Error('not found')
     }
