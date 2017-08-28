170a0c692c7e23e527da8042defe1b7a039c91af
Mark Vayngrib <mark@tradle.io>
2017-08-28 15:34:22 -0400

    enable cors

--- example/server.js
+++ example/server.js
@@ -6,19 +6,25 @@ require('./local-config')
 const express = require('express')
 const expressGraphQL = require('express-graphql')
 const compression = require('compression')
+const cors = require('cors')
+const dynogels = require('dynogels')
 const { createSchema } = require('@tradle/schema-graphql')
 const objects = require('./objects')
 const models = require('./models')
 const { schema } = require('../')({ models, objects })
 
+const debug = require('debug')('dynogels')
+dynogels.log = {
+  info: debug,
+  warn: debug,
+  level: 'info'
+}
+
 const port = 4000
 const app = express()
+app.use(cors())
 // app.use(express.static(__dirname))
-app.use(compression({
-  // threshold: 0,
-  // filter: () => true
-}))
-
+app.use(compression())
 app.use('/', expressGraphQL(req => ({
   schema,
   graphiql: true,
