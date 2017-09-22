56d9927d3453b5cf93404a82fc7558014df63c74
Mark Vayngrib <mark@tradle.io>
2017-09-22 09:52:28 -0400

    bump deps

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const graphql = require('graphql')
-const {
-  constants,
-  createTables,
-  createResolvers
-} = require('@tradle/dynamodb')
-
+const tradleDynamo = require('@tradle/dynamodb')
 const { createSchema } = require('@tradle/schema-graphql')
 
 exports = module.exports = function setup ({ models, objects }) {
-  const tables = createTables({ models, objects })
-  const resolvers = createResolvers({
+  const db = tradleDynamo.db({ models, objects })
+  const resolvers = tradleDynamo.createResolvers({
     objects,
     models,
-    tables
+    db
   })
 
   const { schema, schemas } = createSchema({ models, objects, resolvers })
@@ -21,7 +16,7 @@ exports = module.exports = function setup ({ models, objects }) {
   }
 
   return {
-    tables,
+    tables: db.tables,
     resolvers,
     schema,
     schemas,
